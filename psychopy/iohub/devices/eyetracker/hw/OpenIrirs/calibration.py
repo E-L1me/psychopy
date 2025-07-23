2dd6ebdd401c5e175982eb176cc917bf0c320759
E-L1me <phoenixejf@gmail.com>
2025-07-22 18:53:40 -0700

    compied calibration

--- psychopy/iohub/devices/eyetracker/hw/OpenIrirs/calibration.py
+++ psychopy/iohub/devices/eyetracker/hw/OpenIrirs/calibration.py
@@ -232,5 +232,156 @@ class DPICalibrationProcedure:
 
         self.startCalibrationHook()
 
+        i = 0
+        abort_calibration = False
+        for pt in self.cal_target_list:
+            if abort_calibration:
+                break
+            x, y = left + w * pt[0], bottom h*(1.0-pt[1])
+            start_time = currentTime()
+
+            self.clearAllEventBuffers()
+
+            animate_enable = self.getCalibSetting(['target_attributes', 'animate', 'enable'])
+            animate_expansion_ratio = self.getCalibSetting(['target_attributes', 'animate', 'expansion_ratio'])
+            animate_contract_only = self.getCalibSetting(['target_attributes', 'animate', 'contract_only'])
+
+            while currentTime()-start_time <= target_delay:
+                if animate_enable and i > 0:
+                    t = (currentTime()-start_time) / target_delay
+                    v1 = self.cal_target_list[i-1]
+                    v2 = pt
+                    t = 60.0 * ((1.0 / 10.0) * t ** 5 - (1.0 / 4.0) * t ** 4 + (1.0 / 6.0) * t ** 3)
+                    mx, my = ((1.0 - t) * v1[0] + t * v2[0], (1.0 - t) * v1[1] + t * v2[1])
+                    moveTo = left + w * mx, bottom + h * (1.0 - my)
+                    self.drawCalibrationTarget(moveTo)
+                elif animate_enable is False:
+                    if self.targetClassHasPlayPause and self.targetStim.status == PLAYING:
+                        self.targetStim.pause()
+                    self.window.flip(clearBuffer=True)
+            
+            gevent.sleep(0.001)
+            self.MsgPump()
+            msg = self.getNextMsg()
+            if self.allow_escape and msg == 'QUIT':
+                abort_calibration = True
+                break
+
+            self.resetTargetProperties()
+            if self.targetClassHassPlayPause and self.targetStim.status != PLAYING:
+                self.targetSTim.play()
+            self.drawCalibrationTarget((x,y))
+
+            start_time = currentTime()
+            stim_size = self.targetStim.size[0]
+            min_stim_size = self.targetStim.size[0] / animate_expansion_ratio
+            if hasattr(self.targetStim, 'minSize'):
+                min_stim_size = self.targetStim.minSize[0]
+            
+            while currentTime() - start_time <= target_duration:
+                elapsed_time = currentTime() - start_time 
+                new_size = t = None
+                if animate_contract_only:
+                    t = elapsed_time / target_duration
+                    new_size = stim_size - t * (stim_size - min_stim_size)
+                elif animate_expansion_ratio not in [1, 1.0]:
+                    if elapsed_time <= target_duration / 2:
+                        t = elapsed_time / (target_duration/2)
+                        new_size = stim_size + t * (stim_size * animate_expansion_ratio - stim_size)
+                    else: 
+                        t = (elapsed_time - target_duration/2) / (target_duration / 2)
+                        new_size = stim_size* animate_expansion_ratio - t * (stim_size * animate_expansion_ratio - min_stim_size)
+                if new_size:
+                    self.targetStim.size = new_size, new_size
+                
+                self.targetStim.draw()
+                self.window.flip()
+            
+            if auto_pace is False:
+                while 1: 
+                    if self.targetClassHasPlayPause and self.targetStim.status == PLAYING:
+                        self.targetStim.draw()
+                        self.window.flip()
+                    gevent.sleep(0.001)
+                    self.MsgPump()
+                    msg = self.getNextMsg()
+                    if msg = 'SPACE_KEY_ACTION': 
+                        break
+                    elif self.allow_escape and msg == 'QUIT':
+                        abort_calibration = True
+                        break
+            
+            gevent.sleep(0.001)
+            self.MsgPump()
+            msg = self.getNextMsg()
+            while msg:
+                if self.allow_escape and msg = 'QUIT':
+                    abort_calibration = True
+                    break
+                gevent.sleep(0.001)
+                self.MsgPump()
+                msg = self.getNextMsg()
+            
+            self.registerCalibrationPointHook(pt)
+
+            self.clearCalibrationWindow()
+            self.clearAllEventBuffers()
+            i += 1
         
+        if self.targetClassHasPlayPause:
+            self.targetStim.pause()
+        
+        self.finishCalibrationHook(abort_calibration)
+
+        if abort_calibration is False:
+            self.showFinishedScreen()
+        
+        return not abort_calibration
+    
+    def clearCalibrationWindow(self):
+        self.window.flip(clearBuffer=True)
+    
+    def showIntroScreen(self, text_msg='Press SPACE to Start Calibration; Press ESCAPE to Exit.'):
+
+        self.clearAllEventBuffers()
+
+        while true:
+            self.textLineStim.setText(text_msg)
+            self.textLineStim.draw()
+            self.window.flip()
+
+            msg = self.getNextMsg()
+            if msg == 'SPACE_KEY_ACTION':
+                self.clearAllEventBuffers()
+                return True
+            elif msg == 'QUIT':
+                self.clearAllEventBuffers()
+                return False
+            self.MsgPump()
+            gevent.sleep(0.001)
+    
+    def showFinishedScreen(self, text_msg="Calibration Complete. Press 'SPACE' key to continue."):
+        
+        self.clearAllEventBuffers()
+
+        while True:
+            self.textLineStim.setText(text_msg)
+            self.textLineStim.draw()
+            self.window.flip()
+
+            msg = self.getNextMsg()
+            if msg in ['SPACE_KEY_ACTION', 'QUIT']:
+                self.clearAllEventBuffers()
+                return True
+            
+            self.MsgPump()
+            gevent.sleep(0.001)
+
+    def resetTargetProperties(self):
+        self.targetStim.size = self.originalTargetSize
+
+    def drawCalibrationTarget(self, tp):
+        self.targetStim.setPos(tp)
+        self.targetStim.draw()
+        return self.window.flip(clearBuffer=True)
 
