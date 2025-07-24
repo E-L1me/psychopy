4032cf17c1a1294075471f99ff5bc40188d64f6d
E-L1me <phoenixejf@gmail.com>
2025-07-23 21:49:15 -0700

    changes to calibration

--- psychopy/iohub/devices/eyetracker/hw/OpenIrirs/calibration.py
+++ psychopy/iohub/devices/eyetracker/hw/OpenIrirs/calibration.py
@@ -1,3 +1,5 @@
+from psychopy.iohub.devices.eyetracker.calibration import BaseCalibrationProcedure
+
 from psychopy import visual, layout
 import gevent
 from psychopy.iohub.util import convertCamelToSnake, updateSettings, createCustomCalibrationStim
@@ -9,203 +11,10 @@ from psychopy.constants import PLAYING
 
 currentTime = Computer.getTime
 
-target_position_count = dict(THREE_POINTS=3,
-                             FIVE_POINTS=5,
-                             NINE_POINTS=9,
-                             THIRTEEN_POINTS=13)
-target_positions = dict()
-target_positions[3] = [(0.5, 0.1), (0.1, 0.9), (0.9, 0.9)]
-target_positions[5] = [(0.5, 0.5), (0.1, 0.1), (0.9, 0.1), (0.9, 0.9), (0.1, 0.9)]
-target_positions[9] = [(0.5, 0.5), (0.1, 0.5), (0.9, 0.5), (0.1, 0.1), (0.5, 0.1),
-                       (0.9, 0.1), (0.9, 0.9), (0.5, 0.9), (0.1, 0.9)]
-target_positions[13] = [(0.5, 0.5), (0.1, 0.5), (0.9, 0.5), (0.1, 0.1), (0.5, 0.1),
-                        (0.9, 0.1), (0.9, 0.9), (0.5, 0.9), (0.1, 0.9), (0.25, 0.25),
-                        (0.25, 0.75), (0.75, 0.75), (0.75, 0.25)]
+class DPICalibrationProcedure(BaseCalibrationProcedure):
 
-class DPICalibrationProcedure:
-    OHUB_HEARTBEAT_INTERVAL = 0.050
-    CALIBRATION_POINT_LIST = target_positions[13]
-
-    _keyboard_key_index = KeyboardInputEvent.CLASS_ATTRIBUTE_NAMES.index('key')
-
-    def __init__(self, eyetrackerInterface, calibration_args, allow_escape_in_progress=True):
-        self._eyetracker = eyetrackerInterface
-        self.allow_escape = allow_escape_in_progress
-        self.screenSize = eyetrackerInterface._display_device.getPizelResolution()
-        self.width = self.screenSize[0]
-        self.height = self.screenSize[1]
-        self._ioKeyboard = None
-        self._msg_queue = []
-        self._lastCalibrationOk = False
-        self._device_config = self.eyetracker.getConfiguration() ##This needs to be implemented in the eyetracker class
-        display = self.eyetracker.display_device
-        updateSettings(self._device_config.get('calibration'), calibration_args)
-        self._calibration_args = self.device_config.get('calibration')
-        unit_type = self.getCalibSettng('unit_type')
-        if unit_type is None:
-            unit_type = display.getCoordinateType()
-            self._calibration_args['unit_type'] = unit_type
-        color_type = self.getCalibSetting('color_type')
-        if color_type is None:
-            color_type = display.getColorSpace()
-            self._calibration_args['color_type'] = color_type
-        
-        cal_type = self.getCalibSetting('type')
-
-        if cal_type in target_position_count:
-            num_points = target_postition_count[cal_type]
-            BaseCalibrationProcedure.CALIBRATION_POINT_LIST = target_positions[num_points]
-
-        self.cal_target_list = self.CALIBRATION_POINT_LIST
-
-        self.window = visual.Window(
-            self.screenSize,
-            monitor=display.getPsyschopyMonitorName(),
-            units=unit_type,
-            fullscr=True,
-            allowGUI=True,
-            screen=display.getIndex(),
-            color=self.getCalibSetting(['screen_background_color']),
-            colorSpace=color_type
-        )
-        self.window.setMouseVisible(False)
-        self.window.flip(clearBuffer=True)
-
-        self.createGraphics()
-        self._registerEventMonitors()
-        self._lstMsgPumpTime = currentTime()
-
-        self.clearAllEventBuffers()
-
-    def getCalibSettings(self, setting):
-        if isinstance(setting, str):
-            setting = [setting, ]
-        calibration_args = self._calibration_args
-        if setting:
-            for s in setting[:-1]:
-                calibration_args = calibration_args.get(s)
-            return calibration_args.get(setting[-1])
-    
-    def clearAllEventBuffers(self):
-        self.eyetracker._iohub_server.eventBuffer.clear()
-        for d in self._eyetracker._iohub_server.devices:
-            d.clearEvents()
-
-    def _registerEventMonitors(self):
-        kbDevice = None
-        if self._eyetracker._iohub_server:
-            for dev in self._eyetracker._iohub_server.devices:
-                if dev.__class__.__name__ == 'Keyboard':
-                    kbDevice = dev
-        
-        if kbDevice:
-            eventIds = []
-            for event_class_name in kbDevice.__class__.EVENT_CLASS_NAMES:
-                eventIDs.append(getattr(EC, converCamelToSnake(event_class_name[:-5], False)))
-
-            self._ioKeyboard = kbDevice
-            self._ioKeyboard._addEventListener(self, eventIDs)
-        else: print2err('Warning: %s could not connect to Keyboard device for events.' % self.__class__.__name__)
-
-    def _unregisterEventMonitors(self):
-        if self._ioKeyboard:
-            self._ioKeyboard._removeEventListener(self)
-        
-    def _handleEvent(self, event):
-        event_type_index = DeviceEvent.EVENT_TYPE_ID_INDEX
-        if event[event_type_index] == EC.KEYBOARD_RELEASE:
-            ek = event[self._keyboard_key_index]
-            if isinstance(ek, bytes):
-                ek = ek.decode('utf-8')
-            if ek == ' ' or ek == 'space':
-                self._msg_queue.append('SPACE_KEY_ACTION')
-                self.clearAllEventBuffers()
-            elif ek = 'escape':
-                self._msg_queue.append('QUIT')
-                self.clearAllEventBuffers()
-    
-    def MsgPump(self):
-        if currentTime() - self.lastMsgPumpTime > self.IOHUB_HEARTBEAT_INTERVAL:
-            if self._eyetracker._iohub_server:
-                for dm in self._eyetracker._iohub_server.deviceMonitors:
-                    dm.device._poll()
-                self._eyetracker._iohub_server.processDeviceEvents()
-            self._lastMsgPumpTime = currentTime()
-    
-    def getNextMsg(self):
-        if len(self._msg_queue) > 0:
-            msg = self._msg_queue[0]
-            self._msg_queue = self._msg_queue[1:]
-            return msg
-    
-    def createGraphics(self):
-        """
-        """
-        color_type = self.getCalibSetting('color_type')
-        unit_type = self.getCalibSetting('unit_type')
-
-        def setDefaultCalibrationTarget():
-            radiusPix = self.getCalibSetting(['target_attributes', 'outer_diameter'])/2
-            radiusObj = layout.Size(radiusPix, units=unit_type, win=self.window)
-            radius = getattr(radiusObj, unit_type)[1]
-            innerRadiusPix = self.getCalibSetting(['target_attributes', 'inner_diameter'])/2
-            innerRadiusObj = layout.Size(innerRadiusPix, unit=unit_type, win=self.window)
-            innerRadius = getattr(innerRadiusObj, unit_type)[1]
-
-            self.targetStim = visualTargetStim(
-                self.window, name="CP", style="circles",
-                radius=radius,
-                fillColor=self.getCalibSetting(['target_attributes', 'outer_fill_color']),
-                borderColor=self.getCalibSetting(['target_attributes', 'outer_line_color']),
-                lineWidth=self.getCalibSetting(['target_attributes', 'outer_stroke_width']),
-                innerRadius=innerRadius,
-                innerFillColor=self.getCalibSetting(['target_attributes', 'inner_fill_color']),
-                innerBorderColor=self.getCalibSetting(['target_attributes', 'inner_line_color']),
-                innerLineWidth=self.getCalibSetting(['target_attributes', 'inner_stroke_width']),
-                pos=(0,0),
-                units=unit_type,
-                colorSpace=color_type,
-                autoLog=False
-            )
-
-    if self._calibration_args.get('target_type') = 'CIRCLE_TARGET':
-        setDefaultCalibrationTarget()
-    else:
-        self.targetStim = createCustomCalibrationStim(self.window, self._calibration_args)
-        if self.taretStime is None:
-            setDefaultCalibrationTarget()
-        
-    self.originalTargetSize = self.targetStim.size
-    self.targetClassHasPlayPause = hasattr(self.targetStim, 'play') and hasattr(self.targetStim, 'pause')
-
-    self.imagetitlestim = none
-
-    tctype = color_type
-    tcolor = self.getCalibSetting(['text_color'])
-    if tcolor is None:
-        from psuchopy.iohub.util import complement
-        sbcolor = self.getCalibSetting(['screen_background_color'])
-        if sbcolor is None:
-            sbcolor = self.window.color
-        from psychopy.colors import Color
-        tcolor_obj = Color(sbcolor, color_type)
-        tcolor = complement(*tcolor_obj.rgb255)
-        tctype = 'rgb255'
-    
-    instruction_text = 'Press SPACE to Start Calibration; ESCAPE to Exit.'
-    self.textLineStim = visual.TextSTim(self.window, text=instruction_text,
-                                        pos=(0,0), height=36,
-                                        color=tcolor, colorSpace=tctype,
-                                        units='pix', wrapWidth=self.width * 0.9)
-
-    def startCalibrationHook(self):
-        pass
-
-    def registerCalibrationPointHook(self, pt):
-        pass
-
-    def finishCalibrationHook(self, aborted=False):
-        pass
+    def __init__(self, eyetrackerInterface, calibration_args):
+        BaseCalibrationProcedure.__init(self, eyetrackerInterface, calibration_args, allow_escape_in_progress=True)
 
     def runCalibration(self):
         """Run Calibration Sequence
@@ -214,12 +23,12 @@ class DPICalibrationProcedure:
         if self.showIntroScreen() is False:
             return False
         
-        target_delay = self.getCalibSetting('target_delay')
-        target_duration self.getCalibSetting('target_duration')
-        auto_pace = self.getCalibSetting('auto_pace')
-        randomize_points = self.getCalibSetting('randomize')
+        target_delay = super().getCalibSetting('target_delay')
+        target_duration = super().getCalibSetting('target_duration')
+        auto_pace =  super().getCalibSetting('auto_pace')
+        randomize_points =  super().getCalibSetting('randomize')
         if randomize_points is True:
-            self.cal_target_list = self.CALIBRATION_POINT_LIST[1:]
+            super().cal_target_list = super().CALIBRATION_POINT_LIST[1:]
             import random
             random.seed(None)
             random.shuffle(self.cal_target_list)
@@ -237,7 +46,7 @@ class DPICalibrationProcedure:
         for pt in self.cal_target_list:
             if abort_calibration:
                 break
-            x, y = left + w * pt[0], bottom h*(1.0-pt[1])
+            x, y = left + w * pt[0], bottom + h*(1.0-pt[1])
             start_time = currentTime()
 
             self.clearAllEventBuffers()
@@ -305,7 +114,7 @@ class DPICalibrationProcedure:
                     gevent.sleep(0.001)
                     self.MsgPump()
                     msg = self.getNextMsg()
-                    if msg = 'SPACE_KEY_ACTION': 
+                    if msg == 'SPACE_KEY_ACTION': 
                         break
                     elif self.allow_escape and msg == 'QUIT':
                         abort_calibration = True
@@ -315,7 +124,7 @@ class DPICalibrationProcedure:
             self.MsgPump()
             msg = self.getNextMsg()
             while msg:
-                if self.allow_escape and msg = 'QUIT':
+                if self.allow_escape and msg == 'QUIT':
                     abort_calibration = True
                     break
                 gevent.sleep(0.001)
@@ -376,12 +185,3 @@ class DPICalibrationProcedure:
             
             self.MsgPump()
             gevent.sleep(0.001)
-
-    def resetTargetProperties(self):
-        self.targetStim.size = self.originalTargetSize
-
-    def drawCalibrationTarget(self, tp):
-        self.targetStim.setPos(tp)
-        self.targetStim.draw()
-        return self.window.flip(clearBuffer=True)
-
